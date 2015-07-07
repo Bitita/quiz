2a7882fcac300bdd39dcea6cef3e586d1cecfad0
Bitita <bitisuso@yahoo.com>
2015-07-07 21:06:59 +0100

    Marco de la aplicación

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var partials = require('express-partials');
 
 var routes = require('./routes/index');
 //se quita para quitar la gestión de usuarios
@@ -15,6 +16,8 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+app.use(partials());
 
 
 // uncomment after placing your favicon in /public
